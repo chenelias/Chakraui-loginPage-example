9ee4a26e4c3023d363fa625d4a124c7cc9f9e7e9
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-07-19 19:33:32 +0800

    main

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+
+const App = () => {
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Flex } from '@chakra-ui/react'
+
+ReactDOM.render(
+<Flex height="100vh" alignItems='center' justifyContent='center'>
+  <Flex direction="column">
+
+  </Flex>
+  <App/>
+</Flex>
+
+, document.getElementById('root'))

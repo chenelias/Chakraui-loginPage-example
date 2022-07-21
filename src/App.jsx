cb1fd93e90897e764acdfc4935861ddda6b78413
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-07-21 19:26:30 +0800

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Flex, Heading, Input, Button,ChakraProvider } from "@chakra-ui/react";
+import { Flex, Heading, Input, Button,ChakraProvider ,useColorMode, useColorModeValue} from "@chakra-ui/react";
 
 const App = () => {
-
+  const {toggleColorMode} =useColorMode()
+  const fromBackground = useColorModeValue('gray.100','gray.700')
+  // useColorModeValue('lightmode','darkmode')
+  const frombodybackground = useColorModeValue('purple.200','purple.600')
   return (
-      <Flex height="100vh" alignItems="center" justifyContent="center">
-    <Flex direction="column" background="gray.100" p={10} rounded={15}>
+      <Flex height="100vh" background={frombodybackground} alignItems="center" justifyContent="center">
+    <Flex direction="column" background={fromBackground} p={10} rounded={15}>
       <Heading md={6}>Log in</Heading>
       <Input
         placeholder="react@mail.com"
@@ -22,6 +25,7 @@ const App = () => {
         md={6}
       ></Input>
       <Button colorScheme='teal' >Log in</Button>
+      <Button mt={3} fontSize='sm' onClick={toggleColorMode}>Toggle Color Mode</Button>
     </Flex>
   </Flex>
   )

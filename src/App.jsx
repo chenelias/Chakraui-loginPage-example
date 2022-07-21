e43db95d2547b2e3b3a715f7d005326ef4322213
sunlight-git <76611085+chenelias@users.noreply.github.com>
2022-07-21 17:43:46 +0800

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Flex, Heading, Input, Button,ChakraProvider } from "@chakra-ui/react";
 
 const App = () => {
 
   return (
-    <div>App</div>
+      <Flex height="100vh" alignItems="center" justifyContent="center">
+    <Flex direction="column" background="gray.100" p={10} rounded={15}>
+      <Heading md={6}>Log in</Heading>
+      <Input
+        placeholder="react@mail.com"
+        variant="filled"
+        md={3}
+        my={2}
+        type="email"
+      ></Input>
+      <Input
+        placeholder="**********"
+        variant="filled"
+        my={2}
+        type="password"
+        md={6}
+      ></Input>
+      <Button colorScheme='teal' >Log in</Button>
+    </Flex>
+  </Flex>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { Flex } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { Flex, Heading, Input, Button, ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
-<Flex height="100vh" alignItems='center' justifyContent='center'>
-  <Flex direction="column">
-
-  </Flex>
-  <App/>
-</Flex>
-
-, document.getElementById('root'))
+<ChakraProvider><App/></ChakraProvider>,document.getElementById("root")
+);
